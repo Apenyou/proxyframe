b18d165387d69daa3e98b50c7268fcd75d6c3b8d
gaigaibill <973579088@qq.com>
2020-07-11 00:27:32 +0800

    调研win10 通知弹窗和消息弹窗

--- test.py
+++ test.py
@@ -1,4 +1,4 @@
-
+import time
 def log(func):
     def wrapper(*args, **kw):
         # print('call %s():' % func.__name__)
@@ -10,4 +10,47 @@ def log(func):
 def testdel(a):
     print(123123123)
 
-testdel(6666)
+# testdel(6666)
+
+
+
+
+import tinyWinToast.tinyWinToast
+toast = tinyWinToast.tinyWinToast.Toast()
+toast.setHeroImage("F:\codeprivate\proxyframe\qqq.png")
+toast.setImage("F:\codeprivate\proxyframe\qqq.png")
+toast.setTitle("测试标题", maxLines=1)
+toast.setMessage("哈哈哈哈哈哈哈E", maxLines=1)
+toast.addText("MORE TEXT", maxLines=1)
+toast.setIcon('F:\codeprivate\proxyframe\qqq.png')
+
+toast.show()
+
+##提醒OK消息框
+# win32api.MessageBox(0, "这是一个测试提醒OK消息框", "提醒",win32con.MB_OK)
+# win32api.MessageBox(0, "这是一个测试提醒OK消息框", "提醒22",win32con.M)
+# win32api.SendMessageTimeout(23, "这是一个测试提醒OK消息框", "提醒")
+# win32api.SendMessage( 123, 342)
+##是否信息框
+# win32api.MessageBox(0, "这是一个测试是否信息框", "提醒",win32con.MB_YESNO)
+#
+# ##说明信息框
+# win32api.MessageBox(0, "这是一个测试说明信息框", "提醒",win32con.MB_HELP)
+#
+# ####警告信息框
+# win32api.MessageBox(0, "这是一个测试警告信息框", "提醒",win32con.MB_ICONWARNING)
+#
+# ##疑问信息框
+# win32api.MessageBox(0, "这是一个测试疑问信息框", "提醒",win32con.MB_ICONQUESTION)
+#
+# ##提示信息框
+# win32api.MessageBox(0, "这是一个测试提示信息框", "提醒",win32con.MB_ICONASTERISK)
+#
+# ##确认信息框
+# win32api.MessageBox(0, "这是一个测试确认信息框", "提醒",win32con.MB_OKCANCEL)
+#
+# ##重试信息框
+# win32api.MessageBox(0, "这是一个测试重试信息框", "提醒",win32con.MB_RETRYCANCEL)
+#
+# ##是否取消信息框
+# win32api.MessageBox(0, "这是一个测试是否取消信息框", "提醒",win32con.MB_YESNOCANCEL)
